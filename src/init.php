96d541e53a36ba93aefc437ba487a55a11e9f9b3
tomusborne <tom@edge22.com>
2019-01-09 17:24:36 -0800

    Add another section level

--- src/init.php
+++ src/init.php
@@ -45,6 +45,37 @@ function generate_section_block_enqueue_scripts() {
 	}
 }
 
+function generate_get_nested_section_data( $block, $data ) {
+	// Second level.
+	if ( isset( $block['innerBlocks'] ) && ! empty( $block['innerBlocks'] && is_array( $block['innerBlocks'] ) ) ) {
+		foreach ( $block['innerBlocks'] as $inner_block ) {
+			if ( 'generatepress/section' === $inner_block['blockName'] ) {
+				$data[] = $inner_block['attrs'];
+
+				// Third level.
+				if ( isset( $inner_block['innerBlocks'] ) && ! empty( $inner_block['innerBlocks'] && is_array( $inner_block['innerBlocks'] ) ) ) {
+					foreach ( $inner_block['innerBlocks'] as $inner_block ) {
+						if ( 'generatepress/section' === $inner_block['blockName'] ) {
+							$data[] = $inner_block['attrs'];
+
+							// Fourth level.
+							if ( isset( $inner_block['innerBlocks'] ) && ! empty( $inner_block['innerBlocks'] && is_array( $inner_block['innerBlocks'] ) ) ) {
+								foreach ( $inner_block['innerBlocks'] as $inner_block ) {
+									if ( 'generatepress/section' === $inner_block['blockName'] ) {
+										$data[] = $inner_block['attrs'];
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return $data;
+}
+
 function generate_section_block_data() {
 	if ( ! function_exists( 'has_blocks' ) ) {
 		return;
@@ -74,23 +105,7 @@ function generate_section_block_data() {
 				if ( 'generatepress/section' === $block['blockName'] ) {
 					$data[] = $block['attrs'];
 
-					// Second level.
-					if ( isset( $block['innerBlocks'] ) && ! empty( $block['innerBlocks'] && is_array( $block['innerBlocks'] ) ) ) {
-						foreach ( $block['innerBlocks'] as $inner_block ) {
-							if ( 'generatepress/section' === $inner_block['blockName'] ) {
-								$data[] = $inner_block['attrs'];
-
-								// Third level.
-								if ( isset( $inner_block['innerBlocks'] ) && ! empty( $inner_block['innerBlocks'] && is_array( $inner_block['innerBlocks'] ) ) ) {
-									foreach ( $inner_block['innerBlocks'] as $inner_block ) {
-										if ( 'generatepress/section' === $inner_block['blockName'] ) {
-											$data[] = $inner_block['attrs'];
-										}
-									}
-								}
-							}
-						}
-					}
+					$data = generate_get_nested_section_data( $block, $data );
 				}
 
 				if ( 'core/block' === $block['blockName'] ) {
@@ -105,24 +120,8 @@ function generate_section_block_data() {
 							foreach ( $blocks as $index => $block ) {
 								if ( 'generatepress/section' === $block['blockName'] ) {
 									$data[] = $block['attrs'];
-								}
 
-								// Second level.
-								if ( isset( $block['innerBlocks'] ) && ! empty( $block['innerBlocks'] && is_array( $block['innerBlocks'] ) ) ) {
-									foreach ( $block['innerBlocks'] as $inner_block ) {
-										if ( 'generatepress/section' === $inner_block['blockName'] ) {
-											$data[] = $inner_block['attrs'];
-
-											// Third level.
-											if ( isset( $inner_block['innerBlocks'] ) && ! empty( $inner_block['innerBlocks'] && is_array( $inner_block['innerBlocks'] ) ) ) {
-												foreach ( $inner_block['innerBlocks'] as $inner_block ) {
-													if ( 'generatepress/section' === $inner_block['blockName'] ) {
-														$data[] = $inner_block['attrs'];
-													}
-												}
-											}
-										}
-									}
+									$data = generate_get_nested_section_data( $block, $data );
 								}
 							}
 						}
@@ -167,7 +166,7 @@ function generate_section_block_frontend_css() {
 
 	foreach ( $data as $atts ) {
 		if ( ! isset( $atts['uniqueID'] ) ) {
-			return;
+			continue;
 		}
 
 		$id = 'section-' . $atts['uniqueID'];
