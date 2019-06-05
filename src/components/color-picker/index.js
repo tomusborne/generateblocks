993cd0844d33fa645e698f7bf1a2bb80fad1c72c
tomusborne <tom@edge22.com>
2019-06-05 00:10:43 -0700

    Add color-picker component

---
+++ src/components/color-picker/index.js
@@ -0,0 +1,87 @@
+import classnames from 'classnames';
+
+// Import CSS
+import './editor.scss';
+
+const WPColorPicker = wp.components.ColorPicker;
+
+const { Component } = wp.element;
+
+const { __ } = wp.i18n;
+
+const {
+    Dropdown,
+    Tooltip,
+    BaseControl,
+} = wp.components;
+
+const {
+    ColorPalette,
+} = wp.editor;
+
+export default class ColorPicker extends Component {
+    render() {
+        const {
+            value,
+            onChange,
+            label,
+            alpha = false,
+        } = this.props;
+
+        return (
+            <BaseControl
+                label={ label }
+                className="generatepress-component-color-picker-wrapper"
+            >
+                <Dropdown
+                    className={ classnames( 'components-color-palette__item-wrapper', value ? '' : 'components-color-palette__custom-color' ) }
+                    contentClassName="components-color-palette__picker"
+                    renderToggle={ ( { isOpen, onToggle } ) => (
+                        <Tooltip text={ __( 'Custom color picker' ) }>
+                            <button
+                                type="button"
+                                aria-expanded={ isOpen }
+                                className="components-color-palette__item"
+                                onClick={ onToggle }
+                                aria-label={ __( 'Custom color picker' ) }
+                                style={ { color: value ? value : '' } }
+                            >
+                                <span className="components-color-palette__custom-color-gradient" />
+                            </button>
+                        </Tooltip>
+                    ) }
+                    renderContent={ () => (
+                        <div className="generatepress-component-color-picker">
+                            <WPColorPicker
+                                color={ value }
+                                onChangeComplete={ ( color ) => {
+                                    let colorString;
+
+                                    if ( typeof color.rgb === 'undefined' || color.rgb.a === 1 ) {
+                                        colorString = color.hex;
+                                    } else {
+                                        const { r, g, b, a } = color.rgb;
+                                        colorString = `rgba(${ r }, ${ g }, ${ b }, ${ a })`;
+                                    }
+
+                                    onChange( colorString );
+                                } }
+                                disableAlpha={ ! alpha }
+                            />
+                            <BaseControl
+                                label={ __( 'Color palette' ) }
+                                className="generatepress-component-color-picker-palette"
+                            >
+                                <ColorPalette
+                                    value={ value }
+                                    onChange={ ( color ) => onChange( color ) }
+                                    disableCustomColors={ true }
+                                />
+                            </BaseControl>
+                        </div>
+                    ) }
+                />
+            </BaseControl>
+        );
+    }
+}
