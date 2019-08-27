6004f0a1946d43e3bfc2f2f9fa6f496a13def85f
tomusborne <tom@edge22.com>
2019-08-26 17:18:23 -0700

    Clean up commented code

--- src/utils/get-icon/index.js
+++ src/utils/get-icon/index.js
@@ -82,22 +82,3 @@ export default function getIcon( icon ) {
 		);
 	}
 }
-
-// import allIcons from '../../icons/index';
-//
-// export default function getIcon( icon, returnObject ) {
-//     if ( icon && typeof allIcons[ icon ] !== 'undefined' ) {
-//         // return object for block registration.
-//         if ( returnObject ) {
-//             return {
-//                 foreground: '#C9586C',
-//                 src: allIcons[ icon ],
-//             };
-//         }
-//
-//         // return icon.
-//         return allIcons[ icon ]();
-//     }
-//
-//     return '';
-// }
