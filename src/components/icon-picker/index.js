d8a92271c32cf2814e7ff8fa30f0c64548c13f17
tomusborne <tom@edge22.com>
2019-09-27 23:20:08 -0700

    Add more icon shortcuts

--- src/components/icon-picker/index.js
+++ src/components/icon-picker/index.js
@@ -51,33 +51,6 @@ class IconPicker extends Component {
 		return (
 			<Fragment>
 				<div className="icon-chooser-container">
-					<BaseControl label={ __( 'Social', 'flexblocks' ) }>
-						<ul className="fx-icon-chooser">
-						{
-							Object.keys( socialSvgs ).map( ( svg, i ) => {
-								return (
-									<li key={ `editor-pblock-types-list-item-${ i }` }>
-										<Tooltip text={ ( socialSvgs[ svg ]['label'] ) }>
-											<Button
-												isLarge
-												className="editor-block-list-item-button"
-												onClick={ () => {
-													setAttributes( { [ this.props[ 'attrIcon' ] ]: renderToString( socialSvgs[ svg ]['icon'] ) } );
-													setAttributes( { [ this.props[ 'attrCustomIcon' ] ]: false } );
-												} }
-											>
-												<span className="editor-block-types-list__item-icon">
-													{ socialSvgs[ svg ]['icon'] }
-												</span>
-											</Button>
-										</Tooltip>
-									</li>
-								);
-							} )
-						}
-						</ul>
-					</BaseControl>
-
 					<BaseControl label={ __( 'General', 'flexblocks' ) }>
 						<ul className="fx-icon-chooser">
 						{
@@ -95,6 +68,33 @@ class IconPicker extends Component {
 											>
 												<span className="editor-block-types-list__item-icon">
 													{ generalSvgs[ svg ]['icon'] }
+												</span>
+											</Button>
+										</Tooltip>
+									</li>
+								);
+							} )
+						}
+						</ul>
+					</BaseControl>
+
+					<BaseControl label={ __( 'Social', 'flexblocks' ) }>
+						<ul className="fx-icon-chooser">
+						{
+							Object.keys( socialSvgs ).map( ( svg, i ) => {
+								return (
+									<li key={ `editor-pblock-types-list-item-${ i }` }>
+										<Tooltip text={ ( socialSvgs[ svg ]['label'] ) }>
+											<Button
+												isLarge
+												className="editor-block-list-item-button"
+												onClick={ () => {
+													setAttributes( { [ this.props[ 'attrIcon' ] ]: renderToString( socialSvgs[ svg ]['icon'] ) } );
+													setAttributes( { [ this.props[ 'attrCustomIcon' ] ]: false } );
+												} }
+											>
+												<span className="editor-block-types-list__item-icon">
+													{ socialSvgs[ svg ]['icon'] }
 												</span>
 											</Button>
 										</Tooltip>
@@ -147,29 +147,32 @@ class IconPicker extends Component {
 					</BaseControl>
 				) : '' }
 
-				<SelectControl
-					label={ __( 'Icon Location', 'flexblocks' ) }
-					value={ valueIconLocation }
-					options={ locationOptions }
-					onChange={ ( value ) => {
-						setAttributes( {
-							[ this.props[ 'attrIconLocation' ] ]: value
-						} );
-					} }
-				/>
+				{ ( typeof valueIconLocation !== 'undefined' ) ?
+					<SelectControl
+						label={ __( 'Icon Location', 'flexblocks' ) }
+						value={ valueIconLocation }
+						options={ locationOptions }
+						onChange={ ( value ) => {
+							setAttributes( {
+								[ this.props[ 'attrIconLocation' ] ]: value
+							} );
+						} }
+					/> : null
+				}
 
+				{ ( typeof valueRemoveText !== 'undefined' ) ?
+					<ToggleControl
+						label={ __( 'Remove Text', 'flexblocks' ) }
+						checked={ !! valueRemoveText }
+						onChange={ ( value ) => {
+							setAttributes( {
+								[ this.props[ 'attrRemoveText' ] ]: value
+							} );
+						} }
+					/> : null
+				}
 
-				<ToggleControl
-					label={ __( 'Remove Text', 'flexblocks' ) }
-					checked={ !! valueRemoveText }
-					onChange={ ( value ) => {
-						setAttributes( {
-							[ this.props[ 'attrRemoveText' ] ]: value
-						} );
-					} }
-				/>
-
-				{ !! valueRemoveText ? (
+				{ typeof valueAriaLabel !== 'undefined' && !! valueRemoveText ? (
 					<TextControl
 						label={ __( 'ARIA Label', 'flexblocks' ) }
 						help={ __( 'Helpful to people using screen readers.', 'flexblocks' ) }
