a12dd45918aff65057d18342ebcf13fe7b1d8927
tomusborne <tom@edge22.com>
2020-01-15 10:08:28 -0800

    Fix color picker preview

--- src/components/color-picker/hex-to-rgba.js
+++ src/components/color-picker/hex-to-rgba.js
@@ -9,6 +9,10 @@ export default function hexToRGBA( hex, alpha ) {
 		return '';
 	}
 
+	if ( ! alpha ) {
+		return hex;
+	}
+
 	hex = hex.replace( '#', '' );
 	const r = parseInt( hex.length === 3 ? hex.slice( 0, 1 ).repeat( 2 ) : hex.slice( 0, 2 ), 16 );
 	const g = parseInt( hex.length === 3 ? hex.slice( 1, 2 ).repeat( 2 ) : hex.slice( 2, 4 ), 16 );
