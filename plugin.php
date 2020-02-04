5f8c816088360c4c4c1adda340feaf1bc4d6cda2
tomusborne <tom@edge22.com>
2020-02-04 13:51:47 -0800

    Use constant to load includes

--- plugin.php
+++ plugin.php
@@ -22,13 +22,15 @@ define( 'GENERATEBLOCKS_MODULE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GENERATEBLOCKS_MODULE_DIR_URL', plugin_dir_url( __FILE__ ) );
 
 // Load necessary files.
-require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/general.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/defaults.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/generate-css.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-do-css.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-enqueue-css.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/dashboard.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/functions.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/general.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/defaults.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/generate-css.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/class-do-css.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/class-enqueue-css.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'includes/dashboard.php';
+require_once GENERATEBLOCKS_MODULE_DIR . 'src/blocks.php';
+
 /**
  * Adds a redirect option during plugin activation on non-multisite installs.
  *
