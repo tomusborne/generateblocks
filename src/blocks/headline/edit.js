7ece12907f7656fe6b4d714063fa820eaa0c5e12
Tom Usborne <tom@edge22.com>
2020-10-25 21:16:36 -0700

    Remove span from Headline element type options

--- src/blocks/headline/edit.js
+++ src/blocks/headline/edit.js
@@ -344,14 +344,13 @@ class GenerateBlockHeadline extends Component {
 							value={ element }
 							options={ [
 								{ label: 'paragraph', value: 'p' },
-								{ label: 'div', value: 'div' },
-								{ label: 'span', value: 'span' },
 								{ label: 'h1', value: 'h1' },
 								{ label: 'h2', value: 'h2' },
 								{ label: 'h3', value: 'h3' },
 								{ label: 'h4', value: 'h4' },
 								{ label: 'h5', value: 'h5' },
 								{ label: 'h6', value: 'h6' },
+								{ label: 'div', value: 'div' },
 							] }
 							onChange={ ( value ) => {
 								setAttributes( {
