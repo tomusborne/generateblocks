8f7cd56b98a0215cb6f56004041160057bc7939e
Tom Usborne <tom@edge22.com>
2020-11-05 11:19:22 -0800

    Fix vertical and horizontal gap in tablet/mobile previews

--- src/blocks/grid/css/mobile.js
+++ src/blocks/grid/css/mobile.js
@@ -23,14 +23,14 @@ export default class MobileCSS extends Component {
 		cssObj[ '.gb-grid-wrapper-' + uniqueId + ' > .block-editor-inner-blocks > .block-editor-block-list__layout' ] = [ {
 			'align-items': 'inherit' !== verticalAlignmentMobile ? verticalAlignmentMobile : null,
 			'justify-content': 'inherit' !== horizontalAlignmentMobile ? horizontalAlignmentMobile : null,
-			'margin-left': '-' + ( horizontalGapMobile / 2 ) + 'px',
-			'margin-right': '-' + ( horizontalGapMobile / 2 ) + 'px',
+			'margin-left': horizontalGapMobile || 0 === horizontalGapMobile ? '-' + ( horizontalGapMobile / 2 ) + 'px' : null,
+			'margin-right': horizontalGapMobile || 0 === horizontalGapMobile ? '-' + ( horizontalGapMobile / 2 ) + 'px' : null,
 		} ];
 
 		cssObj[ '.gb-grid-wrapper-' + uniqueId + ' > .block-editor-inner-blocks > .block-editor-block-list__layout > .wp-block' ] = [ {
-			'padding-left': ( horizontalGapMobile / 2 ) + 'px',
-			'padding-right': ( horizontalGapMobile / 2 ) + 'px',
-			'margin-bottom': valueWithUnit( verticalGapMobile, 'px' ),
+			'padding-left': horizontalGapMobile || 0 === horizontalGapMobile ? ( horizontalGapMobile / 2 ) + 'px' : null,
+			'padding-right': horizontalGapMobile || 0 === horizontalGapMobile ? ( horizontalGapMobile / 2 ) + 'px' : null,
+			'margin-bottom': verticalGapMobile || 0 === verticalGapMobile ? valueWithUnit( verticalGapMobile, 'px' ) : null,
 		} ];
 
 		cssObj = applyFilters( 'generateblocks.editor.mobileCSS', cssObj, this.props, 'grid' );
--- src/blocks/grid/css/tablet.js
+++ src/blocks/grid/css/tablet.js
@@ -23,14 +23,14 @@ export default class TabletCSS extends Component {
 		cssObj[ '.gb-grid-wrapper-' + uniqueId + ' > .block-editor-inner-blocks > .block-editor-block-list__layout' ] = [ {
 			'align-items': 'inherit' !== verticalAlignmentTablet ? verticalAlignmentTablet : null,
 			'justify-content': 'inherit' !== horizontalAlignmentTablet ? horizontalAlignmentTablet : null,
-			'margin-left': '-' + ( horizontalGapTablet / 2 ) + 'px',
-			'margin-right': '-' + ( horizontalGapTablet / 2 ) + 'px',
+			'margin-left': horizontalGapTablet || 0 === horizontalGapTablet ? '-' + ( horizontalGapTablet / 2 ) + 'px' : null,
+			'margin-right': horizontalGapTablet || 0 === horizontalGapTablet ? '-' + ( horizontalGapTablet / 2 ) + 'px' : null,
 		} ];
 
 		cssObj[ '.gb-grid-wrapper-' + uniqueId + ' > .block-editor-inner-blocks > .block-editor-block-list__layout > .wp-block' ] = [ {
-			'padding-left': ( horizontalGapTablet / 2 ) + 'px',
-			'padding-right': ( horizontalGapTablet / 2 ) + 'px',
-			'margin-bottom': valueWithUnit( verticalGapTablet, 'px' ),
+			'padding-left': horizontalGapTablet || 0 === horizontalGapTablet ? ( horizontalGapTablet / 2 ) + 'px' : null,
+			'padding-right': horizontalGapTablet || 0 === horizontalGapTablet ? ( horizontalGapTablet / 2 ) + 'px' : null,
+			'margin-bottom': verticalGapTablet || 0 === verticalGapTablet ? valueWithUnit( verticalGapTablet, 'px' ) : null,
 		} ];
 
 		cssObj = applyFilters( 'generateblocks.editor.tabletCSS', cssObj, this.props, 'grid' );
