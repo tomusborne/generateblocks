ddf181c4f9a7466e28e63a9b1578c71619d6361d
Tom Usborne <tom@edge22.com>
2020-12-10 00:20:52 -0800

    Update blocks.asset.php

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '1b23ceac6c833a59d95adc4151f9f245');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '47690c24a325be707b4e52046bf01a94');
