d8999fb2d5dc81a5b90d2b58ee4e7beeee1cae06
Tom Usborne <tom@edge22.com>
2020-12-19 13:59:03 -0800

    Update dist

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-richText'), 'version' => '00e6b3a67bde4e0dbd7221d3504e0fc0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-richText'), 'version' => '0cfebf1bc026d96d4a85104f8afb8cc5');
