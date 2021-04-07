690fce5b0b5d193c96b7e5e7e2168dff7f50832e
Tom Usborne <tom@edge22.com>
2021-04-07 15:33:58 -0700

    Update dist

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-richText'), 'version' => '1fd27759dca4488fd72d32c22b85fc10');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-richText'), 'version' => 'cb42fdc6d8724dfa1fc2e9a73d373dc4');
