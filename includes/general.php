1a1323e7089d074c081af0224962c8e7ec9c9bf1
Tom Usborne <tom@edge22.com>
2021-06-23 08:35:22 -0700

    Improve widget styling

--- includes/general.php
+++ includes/general.php
@@ -255,18 +255,11 @@ add_filter( 'generateblocks_do_content', 'generateblocks_do_widget_styling' );
 function generateblocks_do_widget_styling( $content ) {
 	$widget_blocks = get_option( 'widget_block' );
 
-	return array_reduce(
-		(array) $widget_blocks,
-		function( $content, $block ) {
-			if (
-				isset( $block['content'] )
-				&& preg_match( '/(wp:generateblocks\/)/', $block['content'] )
-			) {
-				$content .= $block['content'];
-			}
+	foreach ( (array) $widget_blocks as $block ) {
+		if ( isset( $block['content'] ) ) {
+			$content .= $block['content'];
+		}
+	}
 
-			return $content;
-		},
-		$content
-	);
+	return $content;
 }
