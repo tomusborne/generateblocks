2bc969b26212615c201b962627fe2f6fbbf8cd15
Tom Usborne <tom@edge22.com>
2021-09-02 19:53:41 -0700

    Validate flex-basis field

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'e0edb8bb73f3821d8ae0a56c774d298d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '2ef864d46c899cc0e3fb02a52841948f');
