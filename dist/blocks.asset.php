4a11ce9cd9a8b3b7839c33bebf942a2c6a0cbd1b
Tom Usborne <tom@edge22.com>
2021-09-09 20:07:46 -0700

    Disable default container appender

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '8513f329ac27f8e0011658faa444dfb9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '20caf80359024a5145fbd2753287e328');
