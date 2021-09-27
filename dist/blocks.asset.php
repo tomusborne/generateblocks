f8e6ac1ff583fe34fc8e94ddec4f50e41c8c0481
Jean Paiva <jeanpaiva29@gmail.com>
2021-09-27 14:01:52 -0300

    Fix uniqueId regenarating on page reload

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '486badd881b8640fb7ad6afbda892293');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '3bda22e1fe7409080dccd54ce59d45b3');
