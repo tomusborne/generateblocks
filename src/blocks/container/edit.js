5e400ed72adb083a04b1d6fd2ae017c6e2a5b80e
Tom Usborne <tom@edge22.com>
2021-10-06 14:39:10 -0700

    Only show empty container guides if not selected

--- src/blocks/container/edit.js
+++ src/blocks/container/edit.js
@@ -446,7 +446,7 @@ class GenerateBlockContainer extends Component {
 				[ `gb-container-${ uniqueId }` ]: true,
 				[ `${ className }` ]: undefined !== className,
 				'gb-container-empty': ! hasChildBlocks,
-				'gb-container-visual-guides': ! hasChildBlocks && ! hasStyling,
+				'gb-container-visual-guides': ! hasChildBlocks && ! hasStyling && ! this.props.isSelected,
 			} ),
 			id: anchor ? anchor : null,
 		};
