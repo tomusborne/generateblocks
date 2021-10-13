1deb4ffcc36cdd4e6215c30db18ffa0b8e7844f9
Jean Paiva <jeanpaiva29@gmail.com>
2021-10-13 19:39:46 -0300

    Build files

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'a6280ee236fc368a9269690ab27689a5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '0acccd2b5b5223031ff4d30ef690f0f8');
