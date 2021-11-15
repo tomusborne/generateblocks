0417c70d604cbc693e66113c5fbb8b0510e8362c
Jean Paiva <jeanpaiva29@gmail.com>
2021-11-15 15:14:28 -0300

    Add isQueryLoop a default false value

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'ec0fdbe5eda2cb6603cba98d7b24ef76');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '66b4c0de25f5dfd49990b56c54e1a751');
--- src/blocks/grid/attributes.js
+++ src/blocks/grid/attributes.js
@@ -14,6 +14,7 @@ export default {
 	},
 	isQueryLoop: {
 		type: 'boolean',
+		default: false,
 	},
 	horizontalGap: {
 		type: 'number',
