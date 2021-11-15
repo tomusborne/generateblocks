950c7485f4a6686e3847dac3008350e6c8e0568c
Tom Usborne <tom@edge22.com>
2021-11-15 10:39:22 -0800

    Make use of useSelect and useDispatch

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'df15fcfed2658d595f59fc9be3878290');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => 'b81cfc45e790f3c524cb1f0aeee95bce');
