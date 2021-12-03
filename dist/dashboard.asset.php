34fec32205afa1891e958451d95205b8fb4fdec5
Jean Paiva <jeanpaiva29@gmail.com>
2021-12-03 00:17:33 -0300

    Build dist

--- dist/dashboard.asset.php
+++ dist/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'd717029ab0824b07c3ba2f3fa44a35e2');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '25586757d2ea85e20ed56f7f37c77c2d');
