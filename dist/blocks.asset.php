10a3db3ada6f47ee1d3a140fc625084c8960d733
Tom Usborne <tom@edge22.com>
2022-02-11 13:00:42 -0800

    Remove string icon attribute
    This was removed in https://github.com/tomusborne/generateblocks/pull/203 but found its way back into our code.

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '0bed77f8a71a359c74c4ec437cffff93');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'bc5a117db6071b39d43c1312bbceaa7c');
