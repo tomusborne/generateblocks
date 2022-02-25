0ee1779f202742d7965bcf80a461dd06f7d0a87a
Tom Usborne <tom@edge22.com>
2022-02-25 14:21:46 -0800

    Add missing blockVersion attribute

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-rich-text'), 'version' => '761fd48865233babd1af9ce8ef491306');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-rich-text'), 'version' => 'fc057499a876ce5448971e5bea645d13');
