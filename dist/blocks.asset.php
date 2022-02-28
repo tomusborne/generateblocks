50c6bcb87b0d520db1db00c3c84f4455da9a1a8a
Tom Usborne <tom@edge22.com>
2022-02-28 14:08:01 -0800

    Fix loop item width in editor

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -3,6 +3,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '2c67b4a1dd0bb3f3af8201bd75f35030');
 =======
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'd8c65327df8973aa720bd912b81b7d3c');
@@ -19,3 +20,6 @@
 =======
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '40690aabf57e3c12ad9e85918acd8a42');
 >>>>>>> Update dist
+=======
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'a65a31f98aa329ac4e0a82ef4d958ef6');
+>>>>>>> Fix loop item width in editor
