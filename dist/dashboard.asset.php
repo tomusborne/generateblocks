13112977dd2889fcf719304ab33ddb71787d3993
Tom Usborne <tom@edge22.com>
2022-03-09 18:53:06 -0800

    Update dist

--- dist/dashboard.asset.php
+++ dist/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'a13c92d39aac2864f7726accae29183a');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'd497b4f2e888eab18006f25f845ae499');
