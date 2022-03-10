0056eebb15a8bd98d8e514986a73e2fddcd821a1
Tom Usborne <tom@edge22.com>
2022-03-09 18:53:00 -0800

    Fix lint

--- src/dashboard.js
+++ src/dashboard.js
@@ -114,9 +114,9 @@ class App extends Component {
 								<TextControl
 									type="number"
 									label={ __( 'Default Container Width', 'generateblocks' ) }
-									help={ !! generateBlocksSettings.gpContainerWidth ?
-										__( 'The default width of the Container block is set by GeneratePress in the Customizer.', 'generateblocks' ) :
-										__( 'The default width of the Container block.', 'generateblocks' )
+									help={ !! generateBlocksSettings.gpContainerWidth
+										? __( 'The default width of the Container block is set by GeneratePress in the Customizer.', 'generateblocks' )
+										: __( 'The default width of the Container block.', 'generateblocks' )
 									}
 									disabled={ !! generateBlocksSettings.gpContainerWidth }
 									value={ generateBlocksSettings.gpContainerWidth || this.getSetting( 'container_width' ) }
