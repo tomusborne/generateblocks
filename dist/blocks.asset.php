2db6f2ca9a79e76820b86a43745f2dfdf0551ca6
Tom Usborne <tom@edge22.com>
2022-03-13 20:07:26 -0700

    Keep block-specific panels open

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-rich-text'), 'version' => '1079a7d605efbd8ea29d8e9b6a253b8a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '8746790c6e41f918fdaf096f8dbaa4f4');
--- src/components/panel-area/index.js
+++ src/components/panel-area/index.js
@@ -1,75 +1,77 @@
 import ApplyFilters from '../apply-filters/';
 
-import {
-	PanelBody,
-} from '@wordpress/components';
+import { PanelBody } from '@wordpress/components';
+import { applyFilters } from '@wordpress/hooks';
+import { useEffect, useState } from '@wordpress/element';
 
-import {
-	Component,
-} from '@wordpress/element';
+export default function PanelArea( props ) {
+	const {
+		title = false,
+		initialOpen = false,
+		icon,
+		className,
+		id,
+		state,
+		showPanel = true,
+		children,
+	} = props;
 
-import {
-	applyFilters,
-} from '@wordpress/hooks';
+	const [ openPanels, setOpenPanels ] = useState( JSON.parse( localStorage.getItem( 'generateblocksPanels' ) ) || [] );
 
-/**
- * Component Class
- */
-export default class PanelArea extends Component {
-	render() {
-		const {
-			title = false,
-			initialOpen = false,
-			icon,
-			className,
-			id,
-			state,
-			showPanel = true,
-			children,
-		} = this.props;
+	useEffect( () => {
+		localStorage.setItem( 'generateblocksPanels', JSON.stringify( openPanels ) );
+	}, [ openPanels ] );
 
-		const show = applyFilters( 'generateblocks.editor.showPanel', showPanel, id, this.props );
+	const show = applyFilters( 'generateblocks.editor.showPanel', showPanel, id, props );
 
-		if ( ! show ) {
-			return null;
-		}
-
-		let hasChildren = true;
-
-		if ( '' === children ) {
-			hasChildren = false;
-		}
-
-		// If we have items in the panel, make sure they're not empty.
-		if ( 'object' === typeof children ) {
-			hasChildren = Object.values( children ).some( ( x ) => ( x !== null && x !== false && x !== '' ) );
-		}
-
-		if ( ! hasChildren ) {
-			return null;
-		}
-
-		return (
-			<ApplyFilters name={ 'generateblocks.panel.' + id } props={ this.props } state={ state }>
-				{ title ? (
-					<PanelBody
-						title={ title }
-						initialOpen={ initialOpen }
-						icon={ icon }
-						className={ className }
-					>
-						{
-							applyFilters( 'generateblocks.editor.panelContents', children, id, this.props )
-						}
-					</PanelBody>
-				) : (
-					<PanelBody>
-						{
-							applyFilters( 'generateblocks.editor.panelContents', children, id, this.props )
-						}
-					</PanelBody>
-				) }
-			</ApplyFilters>
-		);
+	if ( ! show ) {
+		return null;
 	}
+
+	let hasChildren = true;
+
+	if ( '' === children ) {
+		hasChildren = false;
+	}
+
+	// If we have items in the panel, make sure they're not empty.
+	if ( 'object' === typeof children ) {
+		hasChildren = Object.values( children ).some( ( x ) => ( x !== null && x !== false && x !== '' ) );
+	}
+
+	if ( ! hasChildren ) {
+		return null;
+	}
+
+	return (
+		<ApplyFilters name={ 'generateblocks.panel.' + id } props={ props } state={ state }>
+			{ title ? (
+				<PanelBody
+					title={ title }
+					initialOpen={ initialOpen || openPanels.includes( id ) }
+					icon={ icon }
+					className={ className }
+					onToggle={ () => {
+						const panels = JSON.parse( localStorage.getItem( 'generateblocksPanels' ) );
+
+						if ( panels.includes( id ) ) {
+							setOpenPanels( panels.filter( ( panel ) => panel !== id ) );
+						} else {
+							setOpenPanels( [ ...panels, id ] );
+						}
+					} }
+				>
+					{
+						applyFilters( 'generateblocks.editor.panelContents', children, id, props )
+					}
+				</PanelBody>
+			) : (
+				<PanelBody>
+					{
+						applyFilters( 'generateblocks.editor.panelContents', children, id, props )
+					}
+				</PanelBody>
+			) }
+		</ApplyFilters>
+	);
 }
