7c44d5ea64c529f8ddb44bb09466957b53945d48
Tom Usborne <tom@edge22.com>
2022-03-21 12:08:32 -0700

    Make sure both shouldRebuildCSS objects exist

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '45fe58b8b4c8061080e3f6cb3a321635');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '76632a632005065d7f46c101ba77364e');
