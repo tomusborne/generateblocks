f39fce56e2b8b3dc6dbdf1cde1170bcb26a37adf
Tom Usborne <tom@edge22.com>
2022-03-25 10:57:52 -0700

    Add new block icons

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'd7dbdfa9bdee1bc1c962e25f2560fe39');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '68c7f635cb6a32a9de969e50352bd1a2');
--- src/blocks/post-template/index.js
+++ src/blocks/post-template/index.js
@@ -1,8 +1,10 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/block-editor';
 import PostTemplateEdit from './edit';
+import getIcon from '../../utils/get-icon';
 
 registerBlockType( 'generateblocks/post-template', {
+	icon: getIcon( 'post-template' ),
 	edit: PostTemplateEdit,
 	save: () => {
 		return <InnerBlocks.Content />;
