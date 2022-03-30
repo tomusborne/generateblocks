634fc4ccbbba9332a0db54ca3feffd6cc596cd85
Tom Usborne <tom@edge22.com>
2022-03-30 13:24:24 -0700

    Use lodash isEqual for shouldRebuildCSS

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'e702ddb03ff8341be93a0c7b80e47abc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '327dd2dd4ce2bf9ee9eb773254835641');
