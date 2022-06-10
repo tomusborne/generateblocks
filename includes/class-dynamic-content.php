3373002df0424e1be8e8186df09819567c37b285
Tom Usborne <tom@edge22.com>
2022-06-09 20:55:18 -0700

    Tweak: Change image description option value name

--- includes/class-dynamic-content.php
+++ includes/class-dynamic-content.php
@@ -124,7 +124,7 @@ class GenerateBlocks_Dynamic_Content {
 				$content = self::get_image_alt_text( $attributes, $block );
 				break;
 
-			case 'description':
+			case 'image-description':
 				$content = self::get_image_description( $attributes, $block );
 				break;
 		}
@@ -619,7 +619,7 @@ class GenerateBlocks_Dynamic_Content {
 			$id = absint( $attributes['postId'] );
 		}
 
-		$image_content_types = array( 'caption', 'post-title', 'alt-text', 'description' );
+		$image_content_types = array( 'caption', 'post-title', 'alt-text', 'image-description' );
 
 		if ( isset( $attributes['dynamicContentType'] ) ) {
 			if ( in_array( $attributes['dynamicContentType'], $image_content_types ) ) {
