16de519b6a2241095ae9127b0be314a4dd9e8b34
Jean Paiva <jeanpaiva29@gmail.com>
2022-08-23 19:23:51 -0300

    Feature: Add filter for query loop enabled post types

--- src/extend/dynamic-content/components/SelectPostType.js
+++ src/extend/dynamic-content/components/SelectPostType.js
@@ -2,18 +2,24 @@ import { __ } from '@wordpress/i18n';
 import AdvancedSelect from '../../../components/advanced-select';
 import usePostTypes from '../hooks/usePostTypes';
 import { useMemo } from '@wordpress/element';
+import { applyFilters } from '@wordpress/hooks';
 
 export default ( { postType, onChange, value, help } ) => {
 	const postTypes = usePostTypes();
 
+	const enabledPostTypes = useMemo( () => {
+		const enabled = postTypes.filter( ( type ) => ( type.viewable && 'attachment' !== type.slug ) );
+
+		return applyFilters( 'generateBlocks.editor.query-loop.enabled-post-types', enabled, postTypes );
+	}, [ postTypes ] );
+
 	const postTypeOptions = useMemo( () => {
-		return postTypes
-			.filter( ( type ) => ( type.viewable && 'attachment' !== type.slug ) )
+		return enabledPostTypes
 			.reduce( ( result, type ) => {
 				result.push( { value: type.slug, label: type.name } );
 				return result;
 			}, [] );
-	}, [ postTypes ] );
+	}, [ enabledPostTypes ] );
 
 	const selectValue = postTypeOptions.filter( ( option ) => ( option.value === postType || option.value === value ) );
 
