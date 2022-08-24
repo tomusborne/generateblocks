5c2ab26d09ec250b44f96a1eedb9e8766f924805
Tom Usborne <tom@edge22.com>
2022-08-24 09:05:10 -0700

    Fix: Pass dynamic container link to settings

--- includes/blocks/class-container.php
+++ includes/blocks/class-container.php
@@ -768,6 +768,7 @@ class GenerateBlocks_Block_Container {
 		if ( isset( $settings['url'] ) ) {
 			if ( $settings['useDynamicData'] && '' !== $settings['dynamicLinkType'] ) {
 				$attributes['url'] = GenerateBlocks_Dynamic_Content::get_dynamic_url( $settings, $block );
+				$settings['url'] = $attributes['url'];
 			}
 		}
 
