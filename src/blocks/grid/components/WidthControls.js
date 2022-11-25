19cd5a78d421733daa00fe1a291899be505e5030
Tom Usborne <tom@edge22.com>
2022-11-25 09:47:55 -0800

    Feature: Add grid item width controls

---
+++ src/blocks/grid/components/WidthControls.js
@@ -0,0 +1,66 @@
+import { Fragment } from '@wordpress/element';
+import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+import { BaseControl, ButtonGroup, Button } from '@wordpress/components';
+import getAttribute from '../../../utils/get-attribute';
+import { useDeviceType } from '../../../hooks';
+
+function GridItemSettings( content, props ) {
+	const { attributes, setAttributes } = props;
+	const { isGrid, sizing } = attributes;
+	const [ device ] = useDeviceType();
+
+	if ( ! isGrid ) {
+		return content;
+	}
+
+	function getValue( name ) {
+		return sizing && sizing[ getAttribute( name, { attributes, deviceType: device }, true ) ]
+			? sizing[ getAttribute( name, { attributes, deviceType: device }, true ) ]
+			: '';
+	}
+
+	const widths = [
+		{ value: '25%', label: '25' },
+		{ value: '33.33%', label: '33' },
+		{ value: '50%', label: '50' },
+		{ value: '66.66%', label: '66' },
+		{ value: '75%', label: '75' },
+		{ value: '100%', label: '100' },
+	];
+
+	return (
+		<>
+			<BaseControl
+				id="gridItemWidth"
+				label={ __( 'Grid Item Width (%)', 'generateblocks' ) }
+				help={ __( 'More values can be set in the Sizing panel.', 'generateblocks' ) }
+			>
+				<ButtonGroup id="gridItemWidth" className="gblocks-flex-button-group">
+					{
+						Object.values( widths ).map( ( width, index ) => {
+							return (
+								<Fragment key={ 'gridItemWidth' + index }>
+									<Button
+										isPrimary={ width.value === getValue( 'width' ) }
+										onClick={ () => setAttributes( { sizing: { ...sizing, [ getAttribute( 'width', { attributes, deviceType: device }, true ) ]: width.value } } ) }
+									>
+										{ width.label }
+									</Button>
+								</Fragment>
+							);
+						} )
+					}
+				</ButtonGroup>
+			</BaseControl>
+
+			{ content }
+		</>
+	);
+}
+
+addFilter(
+	'generateblocks.editor.containerSettingsPanel',
+	'generateblocks/grid/gridItemSettings',
+	GridItemSettings
+);
