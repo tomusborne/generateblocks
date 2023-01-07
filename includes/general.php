f29a05e76ac08f419e0f371218b6f32b1dec89d3
Tom Usborne <tom@edge22.com>
2023-01-07 13:37:58 -0800

    Tweak: Add tagname back to Headline selector

--- includes/general.php
+++ includes/general.php
@@ -494,9 +494,7 @@ function generateblocks_set_block_css_selectors( $selector, $name, $attributes )
 			$defaults['headline']
 		);
 
-		$include_tagname_default = $blockVersion < 2;
-
-		if ( apply_filters( 'generateblocks_headline_selector_tagname', $include_tagname_default, $attributes ) ) {
+		if ( apply_filters( 'generateblocks_headline_selector_tagname', true, $attributes ) ) {
 			$selector = $settings['element'] . $selector;
 		}
 	}
