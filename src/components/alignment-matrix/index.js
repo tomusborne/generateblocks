bec4edf47b6b56f2d6d609e149c5e6ffbad6a8ed
Jean Paiva <jeanpaiva29@gmail.com>
2023-04-12 18:13:02 -0300

    Feature: Add alignment matrix component

---
+++ src/components/alignment-matrix/index.js
@@ -0,0 +1,90 @@
+import { Dropdown, ToolbarButton } from '@wordpress/components';
+import './styles.scss';
+import { useEffect, useState } from '@wordpress/element';
+import classNames from 'classnames';
+import { __ } from '@wordpress/i18n';
+
+function Matrix( {
+	onChange,
+	activeCell = 'centerCenter',
+	isCompact = false,
+	isOpen = false,
+} ) {
+	const matrixCells = [
+		'topLeft',
+		'topCenter',
+		'topRight',
+		'centerLeft',
+		'centerCenter',
+		'centerRight',
+		'bottomLeft',
+		'bottomCenter',
+		'bottomRight',
+	];
+
+	return (
+		<div
+			className={ classNames( 'gb-matrix', { 'is-compact': isCompact, 'is-open': isOpen } ) }
+		>
+			{ matrixCells.map( ( cell ) => (
+				<div
+					key={ cell }
+					className={ classNames( 'gb-matrix-cell', { active: activeCell === cell } ) }
+					onClick={ !! onChange ? () => onChange( cell ) : undefined }
+					onKeyDown={ !! onChange ? () => onChange( cell ) : undefined }
+					role="presentation"
+				>
+					<span className="gb-matrix-cell-state" />
+				</div>
+			) )
+			}
+		</div>
+	);
+}
+
+function AlignmentMatrix( { options = {}, onChange, defaultOption = 'centerCenter' } ) {
+	const [ activeCell, setActiveCell ] = useState( defaultOption );
+
+	useEffect( () => {
+		onChange( options[ activeCell ] );
+	}, [ activeCell ] );
+
+	return (
+		<Dropdown
+			className="gb-alignment-matrix"
+			contentClassName="gb-alignment-matrix-content"
+			popoverProps={ { variant: 'toolbar', placement: 'bottom-start' } }
+			renderToggle={ ( { isOpen, onToggle } ) => {
+				const openOnArrowDown = ( event ) => {
+					if ( ! isOpen && event.keyCode === 40 ) {
+						event.preventDefault();
+						onToggle();
+					}
+				};
+
+				return (
+					<ToolbarButton
+						onClick={ onToggle }
+						aria-haspopup="true"
+						aria-expanded={ isOpen }
+						onKeyDown={ openOnArrowDown }
+						label={ __( 'Change content position', 'generateblocks' ) }
+						icon={ <Matrix isCompact isOpen={ isOpen } activeCell={ activeCell } /> }
+						showTooltip
+						disabled={ false }
+					/>
+				);
+			} }
+			renderContent={ () => (
+				<Matrix
+					activeCell={ activeCell }
+					onChange={ ( selectedCell ) => {
+						setActiveCell( selectedCell );
+					} }
+				/>
+			) }
+		/>
+	);
+}
+
+export default AlignmentMatrix;
