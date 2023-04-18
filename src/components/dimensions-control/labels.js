0f44605ee79e5f559997897f5f2c9251d803c23b
Tom Usborne <tom@edge22.com>
2023-04-18 16:34:39 -0600

    Add labels

---
+++ src/components/dimensions-control/labels.js
@@ -0,0 +1,10 @@
+export const labels = {
+	marginTop: 'Margin Top',
+	marginRight: 'Margin Right',
+	marginBottom: 'Margin Bottom',
+	marginLeft: 'Margin Left',
+	paddingTop: 'Padding Top',
+	paddingRight: 'Padding Right',
+	paddingBottom: 'Padding Bottom',
+	paddingLeft: 'Padding Left',
+};
