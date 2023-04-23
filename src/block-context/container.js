40e60eeb103e7cda2c44464614e69b7f420e964b
Tom Usborne <tom@edge22.com>
2023-04-22 20:22:35 -0600

    Tweak: Improve Dimensions control

--- src/block-context/container.js
+++ src/block-context/container.js
@@ -50,24 +50,20 @@ const containerContext = defaultsDeep( {
 			enabled: true,
 			dimensions: [
 				{
-					type: 'padding',
 					label: __( 'Padding', 'generateblocks' ),
-					units: [ 'px', 'em', '%' ],
+					attributes: [ 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft' ],
 				},
 				{
-					type: 'margin',
 					label: __( 'Margin', 'generateblocks' ),
-					units: [ 'px', 'em', '%' ],
+					attributes: [ 'marginTop', 'marginRight', 'marginBottom', 'marginLeft' ],
 				},
 				{
-					type: 'borderSize',
 					label: __( 'Border Size', 'generateblocks' ),
-					units: [ 'px' ],
+					attributes: [ 'borderSizeTop', 'borderSizeRight', 'borderSizeBottom', 'borderSizeLeft' ],
 				},
 				{
-					type: 'borderRadius',
 					label: __( 'Border Radius', 'generateblocks' ),
-					units: [ 'px', 'em', '%' ],
+					attributes: [ 'borderRadiusTopLeft', 'borderRadiusTopRight', 'borderRadiusBottomRight', 'borderRadiusBottomLeft' ],
 				},
 			],
 		},
---
+++ src/components/dimensions/labels.js
@@ -0,0 +1,16 @@
+import { __ } from '@wordpress/i18n';
+
+export const labels = {
+	default: [
+		__( 'Top', 'generateblocks' ),
+		__( 'Right', 'generateblocks' ),
+		__( 'Bottom', 'generateblocks' ),
+		__( 'Left', 'generateblocks' ),
+	],
+	borderRadius: [
+		__( 'Top Left', 'generateblocks' ),
+		__( 'Top Right', 'generateblocks' ),
+		__( 'Bottom Right ', 'generateblocks' ),
+		__( 'Bottom Left', 'generateblocks' ),
+	],
+};
