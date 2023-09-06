c67d6096e2cd2414e0b2b3145d73ecc8743858ff
Jean Paiva <JeanPaiva@users.noreply.github.com>
2023-09-06 13:24:56 -0300

    Feature: Import GBP required components for the library

--- src/blocks/pattern-library/components/library-layout.js
+++ src/blocks/pattern-library/components/library-layout.js
@@ -9,11 +9,58 @@ import { useDispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { PatternDetails } from './pattern-details';
+import apiFetch from '@wordpress/api-fetch';
+import { useState } from '@wordpress/element';
+
+function RequiredComponents( { isActive, children } ) {
+	const [ showRefresh, setShowRefresh ] = useState( false );
+	return (
+		<>
+			{ isActive &&
+				<div style={ { maxWidth: '450px', margin: '0 auto' } }>
+					<h3>In order to use the library you need to import required components.</h3>
+					<p>Click the button below to import. Read more.</p>
+					{ ! showRefresh &&
+						<Button
+							variant="primary"
+							onClick={ () => {
+								apiFetch( {
+									path: '/generateblocks-pro/v1/pattern-library/import-required-components',
+									method: 'GET',
+								} ).then( () => {
+									setShowRefresh( true );
+								} );
+							} }
+						>
+							Import
+						</Button>
+					}
+					{ showRefresh &&
+						<Button
+							variant="secondary"
+							onClick={ () => {
+								window.location.reload();
+							} }
+						>
+							Refresh page
+						</Button>
+					}
+				</div>
+			}
+
+			{ ! isActive && children }
+		</>
+	);
+}
 
 export default function LibraryLayout() {
-	const { clientId, activePatternId, setActivePatternId, patterns } = useLibrary();
+	const { clientId, activeLibrary, activePatternId, setActivePatternId, patterns } = useLibrary();
 	const { removeBlock } = useDispatch( blockEditorStore );
 	const activePattern = patterns.find( ( pattern ) => activePatternId === pattern.id );
+	const requiredMessageIsActive = (
+		'gb_default_pro_library' === activeLibrary &&
+		! generateBlocksInfo.hasImportedRequiredComponents
+	);
 
 	return (
 		<div className="pattern-library">
@@ -55,22 +102,24 @@ export default function LibraryLayout() {
 			</div>
 
 			<div className="pattern-library-layout">
-				<div className="library-sidebar">
-					{ ! activePatternId &&
-						<>
-							<PatternSearch />
-							<CategoryList />
-						</>
-					}
-				</div>
-				<div
-					className="library-content"
-					style={ {
-						paddingLeft: !! activePatternId ? 0 : null,
-					} }
-				>
-					<PatternList />
-				</div>
+				<RequiredComponents isActive={ requiredMessageIsActive }>
+					<div className="library-sidebar">
+						{ ! activePatternId &&
+							<>
+								<PatternSearch />
+								<CategoryList />
+							</>
+						}
+					</div>
+					<div
+						className="library-content"
+						style={ {
+							paddingLeft: !! activePatternId ? 0 : null,
+						} }
+					>
+						<PatternList />
+					</div>
+				</RequiredComponents>
 			</div>
 		</div>
 	);
