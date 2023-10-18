804f6c3fa0a786f7788ad867b2b5f176eeebe3e4
David Gwyer <d.v.gwyer@gmail.com>
2023-10-18 11:20:24 +0100

    dist folder removed

--- dist/blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '894da972eabaeebf820c');
--- dist/dashboard.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5f3d32ee5f30ee6d53c2');
