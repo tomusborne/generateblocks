091af98a04d02afe05ce101ea9943e65334818ea
Ian Svoboda <ian@iansvoboda.com>
2023-12-18 13:42:07 -0500

    Add indicator to typography panel

--- src/extend/inspector-control/controls/typography/components/letter-spacing.js
+++ src/extend/inspector-control/controls/typography/components/letter-spacing.js
@@ -1,10 +1,10 @@
 import { __ } from '@wordpress/i18n';
 import UnitControl from '../../../../../components/unit-control';
 
-export default function LetterSpacing( { value, placeholder, onChange, defaultUnit } ) {
+export default function LetterSpacing( { value, placeholder, onChange, defaultUnit, label } ) {
 	return (
 		<UnitControl
-			label={ __( 'Letter Spacing', 'generateblocks' ) }
+			label={ label }
 			id="gblocks-letter-spacing"
 			defaultUnit={ defaultUnit }
 			value={ value }
