79acf805bc2e649cbbd0f6a3d19778e32bc6afef
Ian Svoboda <ian@iansvoboda.com>
2024-04-08 15:40:56 -0400

    Delete test block

--- src/blocks/test-interactive-block/my-first-interactive-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd566fbe52ab32e328e42');
--- src/blocks/test-interactive-block/my-first-interactive-block/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '14b981d5b2a7ca6ef0ef', 'type' => 'module');
