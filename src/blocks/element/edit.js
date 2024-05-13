73c2113ae17fbe012ae1fcbc6b4d3e247c9ae5dc
Tom Usborne <tom@edge22.com>
2024-05-13 14:11:25 -0600

    Add block styles

--- src/blocks/element/edit.js
+++ src/blocks/element/edit.js
@@ -1,15 +1,41 @@
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
-import { useEffect } from '@wordpress/element';
+import { useBlockProps, useInnerBlocksProps, InspectorControls } from '@wordpress/block-editor';
+import { useEffect, useMemo } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
+import { BlockStyles, withUniqueId, useUpdateEditorStyleCSS } from '@edge22/block-styles';
+import { getCss } from '@edge22/styles-builder';
+import { useSelect, useDispatch } from '@wordpress/data';
+import BlockAppender from './components/BlockAppender.jsx';
+import { currentStyleStore, stylesStore, atRuleStore, nestedRuleStore, tabsStore } from '../../store/block-styles';
+import { defaultAtRules } from '../../utils/defaultAtRules.js';
 
-export function Edit( props ) {
+function EditBlock( props ) {
 	const {
 		attributes,
 		setAttributes,
+		clientId,
+		isSelected,
 	} = props;
 
 	const {
 		tagName,
+		className,
+		styles = {},
+		uniqueId,
+		css,
 	} = attributes;
+
+	const classNames = [];
+	const { getStyles } = useSelect( stylesStore );
+	const { addStyle } = useDispatch( stylesStore );
+	const updateEditorCSS = useUpdateEditorStyleCSS();
+
+	if ( className ) {
+		classNames.push( className );
+	}
+
+	if ( Object.keys( styles ).length ) {
+		classNames.push( `gb-element-${ uniqueId }` );
+	}
 
 	useEffect( () => {
 		if ( ! tagName ) {
@@ -17,13 +43,79 @@ export function Edit( props ) {
 		}
 	}, [ tagName ] );
 
+	const selector = useMemo( () => {
+		if ( ! uniqueId ) {
+			return '';
+		}
+
+		return '.gb-element-' + uniqueId;
+	}, [ uniqueId ] );
+
+	function onStyleChange( property, value = '', atRuleValue = '', nestedRuleValue = '' ) {
+		addStyle( property, value, atRuleValue, nestedRuleValue );
+
+		const updatedStyles = getStyles();
+		setAttributes( { styles: updatedStyles } );
+	}
+
+	function getStyleValue( property, nestedRuleValue = '' ) {
+		if ( ! nestedRuleValue ) {
+			return styles?.[ property ] ?? '';
+		}
+
+		return styles?.[ nestedRuleValue ]?.[ property ] ?? '';
+	}
+
+	useEffect( () => {
+		if ( ! selector ) {
+			return;
+		}
+
+		( async function() {
+			const generateCss = await getCss( selector, styles );
+			setAttributes( { css: generateCss } );
+		}() );
+	}, [ JSON.stringify( styles ), selector ] );
+
+	useEffect( () => {
+		if ( ! selector ) {
+			return;
+		}
+
+		updateEditorCSS( selector, css );
+	}, [ css, selector ] );
+
 	const blockProps = useBlockProps();
-	const innerBlocksProps = useInnerBlocksProps( blockProps );
+	const innerBlocksProps = useInnerBlocksProps(
+		blockProps,
+		{
+			renderAppender: () => <BlockAppender clientId={ clientId } isSelected={ isSelected } attributes={ attributes } />,
+		}
+	);
 	const TagName = tagName || 'div';
 
 	return (
 		<>
+			<InspectorControls>
+				<BlockStyles
+					selector={ selector }
+					onStyleChange={ onStyleChange }
+					setAttributes={ setAttributes }
+					styles={ styles }
+					css={ css }
+					stores={ { currentStyleStore, stylesStore, atRuleStore, nestedRuleStore, tabsStore } }
+					defaultAtRules={ defaultAtRules }
+				>
+					test
+				</BlockStyles>
+			</InspectorControls>
 			<TagName { ...innerBlocksProps } />
 		</>
 	);
 }
+
+const Edit = compose(
+	withUniqueId
+)( EditBlock );
+
+export { Edit };
--- src/blocks/element/index.js
+++ src/blocks/element/index.js
@@ -3,6 +3,7 @@ import { Edit } from './edit';
 import metadata from './block.json';
 import getIcon from '../../utils/get-icon';
 import { Save } from './save';
+import './index.scss';
 
 registerBlockType( metadata.name, {
 	edit: Edit,
