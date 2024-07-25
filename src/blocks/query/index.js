63acb316f3fb86ba56d4870b6632b652f6b6c119
Ian Svoboda <ian@iansvoboda.com>
2024-07-25 11:08:15 -0400

    Add unstaged changes

--- src/blocks/query/index.js
+++ src/blocks/query/index.js
@@ -2,14 +2,7 @@ import { registerBlockType, registerBlockVariation } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/block-editor';
 import { Edit } from './edit';
 import metadata from './block.json';
-
-export function queryIcon() {
-	return (
-		<svg className="gb-block-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><path d="M106.63,152.13l-8.69,9.81a48,48,0,1,1,0-67.88l60.12,67.88a48,48,0,1,0,0-67.88l-8.69,9.81" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" /></svg>
-	);
-}
-
-console.log( 'registering generateblocks/query ' );
+import { getIcon } from '@utils';
 
 registerBlockType( metadata, {
 	edit: Edit,
@@ -22,7 +15,7 @@ registerBlockVariation(
 	{
 		title: 'Query',
 		name: 'query',
-		icon: queryIcon,
+		icon: getIcon('query'),
 		isDefault: true,
 		description: 'Build a series of tabs.',
 		attributes: {
