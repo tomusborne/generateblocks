d7a153acb15fc5c35f0e9102ed87bdb4236a7656
Tom Usborne <tom@edge22.com>
2024-09-13 08:44:17 -0600

    Update icons

--- src/blocks/text/index.js
+++ src/blocks/text/index.js
@@ -6,16 +6,12 @@ import metadata from './block.json';
 import { Save } from './save';
 import { getElementType } from '../element/utils/getElementType';
 import { DynamicTag } from './components/DynamicTag';
-import getIcon from '@utils/get-icon';
-
-export function textIcon() {
-	return <svg className="gb-block-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><line x1="128" y1="56" x2="128" y2="200" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12" /><polyline points="56 88 56 56 200 56 200 88" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12" /><line x1="96" y1="200" x2="160" y2="200" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12" /></svg>;
-}
+import { getIcon } from '@utils';
 
 registerBlockType( metadata, {
 	edit: Edit,
 	save: Save,
-	icon: textIcon,
+	icon: getIcon( 'text' ),
 } );
 
 registerBlockVariation(
