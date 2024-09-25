3aad500452c7df20b7dea54266f2e87e0cf71471
Tom Usborne <tom@edge22.com>
2024-09-25 17:11:29 -0600

    Tweak: Update Query Loop icons

--- src/blocks/looper/index.js
+++ src/blocks/looper/index.js
@@ -7,5 +7,5 @@ import { Save } from './save';
 registerBlockType( metadata, {
 	edit: Edit,
 	save: Save,
-	icon: getIcon( 'query' ),
+	icon: getIcon( 'query-loop' ),
 } );
--- src/blocks/query-no-results/index.js
+++ src/blocks/query-no-results/index.js
@@ -5,7 +5,9 @@ import metadata from './block.json';
 
 export function noResultsIcon() {
 	return (
-		<svg className="gb-block-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><circle cx="128" cy="128" r="88" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" /><line x1="208" y1="40" x2="48" y2="216" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" /></svg>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="gblocks-block-icon" aria-hidden="true" focusable="false">
+			<path d="M12,4C16.415,4 20,7.585 20,12C20,16.415 16.415,20 12,20C7.585,20 4,16.415 4,12C4,7.585 7.585,4 12,4ZM17.181,7.676L7.676,17.181C8.847,18.16 10.355,18.75 12,18.75C15.725,18.75 18.75,15.725 18.75,12C18.75,10.355 18.16,8.847 17.181,7.676ZM16.324,6.819C15.153,5.84 13.645,5.25 12,5.25C8.275,5.25 5.25,8.275 5.25,12C5.25,13.645 5.84,15.153 6.819,16.324L16.324,6.819Z" />
+		</svg>
 	);
 }
 
