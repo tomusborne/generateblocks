20af8d1e8b017dff69f4853f6b55680f0ae4f0a7
Ian Svoboda <ian@iansvoboda.com>
2024-10-15 22:13:34 -0400

    Added toolbar buttons for adding pagination and no results blocks

---
+++ src/blocks/query/components/PaginationToolbar.jsx
@@ -0,0 +1,37 @@
+import { BlockControls } from '@wordpress/block-editor';
+import { createBlocksFromInnerBlocksTemplate } from '@wordpress/blocks';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+
+import getIcon from '@utils/get-icon';
+import { PAGINATION_TEMPLATE, NO_RESULTS_TEMPLATE } from '../templates';
+
+const paginationIcon = getIcon( 'pagination' );
+const noResultsIcon = getIcon( 'no-results' );
+
+export function PaginationToolbar( { clientId } ) {
+	const { insertBlocks } = useDispatch( 'core/block-editor' );
+	return (
+		<BlockControls>
+			<ToolbarGroup>
+				<ToolbarButton
+					icon={ paginationIcon }
+					label={ __( 'Add Pagination', 'generateblocks' ) }
+					onClick={ () => {
+						insertBlocks( createBlocksFromInnerBlocksTemplate( [ PAGINATION_TEMPLATE ] ), undefined, clientId );
+					} }
+					showTooltip
+				/>
+				<ToolbarButton
+					icon={ noResultsIcon }
+					label={ __( 'Add No Results', 'generateblocks' ) }
+					onClick={ () => {
+						insertBlocks( createBlocksFromInnerBlocksTemplate( [ NO_RESULTS_TEMPLATE ] ), undefined, clientId );
+					} }
+					showTooltip
+				/>
+			</ToolbarGroup>
+		</BlockControls>
+	);
+}
