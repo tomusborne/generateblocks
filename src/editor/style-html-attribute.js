e600dcf5500b04c2073c7539703f4e5f3406e858
Ian Svoboda <ian@iansvoboda.com>
2024-12-22 21:36:08 -0500

    WIP - Client side caching

--- src/editor/style-html-attribute.js
+++ src/editor/style-html-attribute.js
@@ -1,15 +1,54 @@
 import { addFilter } from '@wordpress/hooks';
 import { replaceTags } from '../dynamic-tags/utils';
 
+const cache = {};
+
+function getCacheKey( clientId, context ) {
+	const {
+		'generateblocks/loopIndex': loopIndex,
+		'generateblocks/loopPreviewId': previewId,
+		postId,
+	} = context;
+
+	let key = '';
+
+	if ( loopIndex ) {
+		key += `${ loopIndex }_`;
+	}
+
+	if ( previewId ) {
+		key += `${ previewId }_`;
+	} else if ( postId ) {
+		key += `${ postId }_`;
+	}
+
+	key += clientId;
+
+	return key;
+}
+
 addFilter(
 	'generateblocks.editor.htmlAttributes.style',
 	'generateblocks/styleWithReplacements',
 	async( style, props ) => {
-		const { context } = props;
+		const { context, clientId } = props;
 
 		// Check if any replacements need to be made
-		if ( ! style.includes( '{{' ) ) {
+		if ( ! style.includes( '{{' ) || ! style ) {
 			return style;
+		}
+
+		const blockCacheKey = getCacheKey( clientId, context );
+
+		// Prime the cache for this block.
+		if ( ! cache[ blockCacheKey ] ) {
+			cache[ blockCacheKey ] = {};
+		}
+
+		// Get the cached result if available.
+		if ( cache[ clientId ][ style ] ) {
+			console.log( 'Using cached data', cache[ clientId ][ style ] );
+			return cache[ style ];
 		}
 
 		const replacements = await replaceTags( style, context );
@@ -17,6 +56,10 @@ addFilter(
 		if ( ! replacements.length ) {
 			return style;
 		}
+
+		// Cache the result.
+		cache[ clientId ][ style ] = replacements;
+		console.log( 'Cache miss, setting data', cache[ clientId ][ style ] );
 
 		const withReplacements = replacements.reduce( ( acc, { original, replacement, fallback } ) => {
 			if ( ! replacement ) {
--- src/hoc/withDynamicTag.js
+++ src/hoc/withDynamicTag.js
@@ -2,11 +2,38 @@ import { useState, useEffect } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { replaceTags } from '../dynamic-tags/utils';
 
+const cache = {};
+
+function getCacheKey( clientId, context ) {
+	const {
+		'generateblocks/loopIndex': loopIndex,
+		'generateblocks/loopPreviewId': previewId,
+		postId,
+	} = context;
+
+	let key = '';
+
+	if ( loopIndex ) {
+		key += `${ loopIndex }_`;
+	}
+
+	if ( previewId ) {
+		key += `${ previewId }_`;
+	} else if ( postId ) {
+		key += `${ postId }_`;
+	}
+
+	key += clientId;
+
+	return key;
+}
+
 export function withDynamicTag( WrappedComponent ) {
 	return ( ( props ) => {
 		const {
 			context,
 			attributes,
+			clientId,
 		} = props;
 
 		const {
@@ -18,6 +45,11 @@ export function withDynamicTag( WrappedComponent ) {
 		const [ dynamicTagValue, setDynamicTagValue ] = useState( '' );
 		const [ contentMode, setContentMode ] = useState( 'edit' );
 		const isSavingPost = useSelect( ( select ) => select( 'core/editor' ).isSavingPost() );
+		const blockCacheKey = getCacheKey( clientId, context );
+
+		if ( ! cache[ blockCacheKey ] ) {
+			cache[ blockCacheKey ] = {};
+		}
 
 		const getContentValue = () => {
 			if ( 'img' === tagName ) {
@@ -43,14 +75,27 @@ export function withDynamicTag( WrappedComponent ) {
 				return;
 			}
 
+			console.log( contentMode );
+
+			if ( cache[ blockCacheKey ][ contentValue ] ) {
+				console.log( 'Using cached data', cache[ blockCacheKey ][ contentValue ] );
+				setDynamicTagValue( cache[ blockCacheKey ][ contentValue ] );
+				return;
+			}
+
 			async function fetchData() {
 				const response = await replaceTags( contentValue, context );
 
+				console.log( 'cache miss, setting cache', cache[ blockCacheKey ][ contentValue ] );
+
 				setDynamicTagValue( response );
+
+				// Cache the response.
+				cache[ blockCacheKey ][ contentValue ] = response;
 			}
 
 			fetchData();
-		}, [ contentValue, contentMode, context, tagName, isSavingPost ] );
+		}, [ contentValue, contentMode, context, tagName, isSavingPost, blockCacheKey ] );
 
 		return (
 			<WrappedComponent
