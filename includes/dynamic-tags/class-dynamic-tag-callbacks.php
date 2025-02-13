68bcff497a17ca0aac97b8816f40d672c8c5e3c1
tdrayson <taylor@drayson.co.uk>
2025-02-13 19:06:35 +0000

    Add url query param support

--- includes/dynamic-tags/class-dynamic-tag-callbacks.php
+++ includes/dynamic-tags/class-dynamic-tag-callbacks.php
@@ -826,4 +826,39 @@ class GenerateBlocks_Dynamic_Tag_Callbacks extends GenerateBlocks_Singleton {
 
 		return self::output( $output, $options, $instance );
 	}
+
+	/**
+	 * Get a URL query parameter value.
+	 *
+	 * @param array  $options The options.
+	 * @param array  $block The block.
+	 * @param object $instance The block instance.
+	 * @return string
+	 */
+	public static function get_url_param( $options, $block, $instance ) {
+		if ( empty( $options['key'] ) ) {
+			return self::output( '', $options, $instance );
+		}
+
+		// Sanitize the key name
+		$key = sanitize_key( $options['key'] );
+
+		// Get the default value if set
+		$default = isset( $options['default'] ) ? sanitize_text_field( $options['default'] ) : '';
+
+		// Check if parameter exists and get its value
+		if ( ! isset( $_GET[ $key ] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			return self::output( $default, $options, $instance );
+		}
+
+		// Sanitize the value based on context
+		$raw_value = wp_unslash( $_GET[ $key ] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+
+		// Handle arrays
+		$value = is_array( $raw_value ) 
+			? implode( $options['delimiter'], array_map( 'sanitize_text_field', $raw_value ) ) 
+			: sanitize_text_field( $raw_value );
+
+		return self::output( $value, $options, $instance );
+	}
 }
