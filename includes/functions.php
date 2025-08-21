d31012d15493b75d89921c32c3a95f82c33d439a
Tom Usborne <tom@edge22.com>
2025-08-21 10:48:25 -0600

    Simplify shortcode parse in URL fields

--- includes/functions.php
+++ includes/functions.php
@@ -2130,6 +2130,10 @@ function generateblocks_get_processed_html_attributes( $html ) {
 function generateblocks_get_escaped_html_attribute( $name, $value ) {
 	$url_fields = [ 'src', 'href' ];
 
+	if ( in_array( $name, $url_fields, true ) && ! empty( $value ) ) {
+		$value = do_shortcode( $value ); // esc_url() escapes shortcodes, so we need to do this first.
+	}
+
 	return in_array( $name, $url_fields, true )
 		? esc_url( $value )
 		: esc_attr( $value );
@@ -2172,8 +2176,7 @@ function generateblocks_with_escaped_attributes( $content, $args = [] ) {
 
 		foreach ( $processor->get_attribute_names_with_prefix( '' ) as $name ) {
 			$attribute_value = $processor->get_attribute( $name );
-			$attribute_value = do_shortcode( $attribute_value ); // Do shortcodes before we escape the value.
-			$escaped_value   = generateblocks_get_escaped_html_attribute( $name, $attribute_value );
+			$escaped_value = generateblocks_get_escaped_html_attribute( $name, $attribute_value );
 
 			// WordPress strips out the `download` value by default, even though it's acceptable to give it a value.
 			// If we have a `download` attribute, let's re-add the value if it exists.
